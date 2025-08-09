e8b1eaa0711d35e13177a411d96b24c26e5fcbfb
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-09 21:25:07 +0000

    Add custom exceptions for grading system: InvalidScoreFormatException and MissingFieldException

---
+++ GradingSystem/GradingSystem/InvalidScoreFormatException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    public class InvalidScoreFormatException : Exception
+    {
+        public InvalidScoreFormatException()
+            : base("The score format is invalid. Please ensure the score is a valid number between 0 and 100")
+        {
+        }
+        public InvalidScoreFormatException(string message)
+            : base(message)
+        {
+        }
+        public InvalidScoreFormatException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ GradingSystem/GradingSystem/MisiingFieldException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    public class MisiingFieldException : Exception
+    {
+        public MisiingFieldException()
+            : base("A required field is missing. Please ensure all fields are filled out correctly.")
+        {
+        }
+        public MisiingFieldException(string message)
+            : base(message)
+        {
+        }
+        public MisiingFieldException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
