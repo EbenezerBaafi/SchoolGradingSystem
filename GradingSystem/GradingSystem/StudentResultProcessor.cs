b757c7c3c91e2144ddbf60c11c0b911b2cb833c1
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-11 19:31:59 +0000

    feat: Implement StudentResultProcessor with file reading, data validation, and custom exception handling for grading system

---
+++ GradingSystem/GradingSystem/StudentResultProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    public class StudentResultProcessor
+    {
+        public   List<Student> ReadStudentsFromFile(string inputFilePath) 
+        {
+            var students = new List<Student>();
+
+            using (StreamReader sr = new StreamReader(inputFilePath))
+            {
+                string line;
+                int LineNumber = 0;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    LineNumber++;
+                    string[] parts = line.Split(',');
+
+                    // validate the number of fields
+                    if (parts.Length != 3)
+                    {
+                        throw new MisiingFieldException($"Line {LineNumber}: Expected 3 fields but found {parts.Length}.");
+                    }
+
+                    string Id = parts[0].Trim();
+                    string FullName = parts[1].Trim();
+                    string Score = parts[2].Trim();
+
+                    // Try converting Id and Score to integers
+                    if (!int.TryParse(Id, out int id))
+                    {
+                        throw new InvalidScoreFormatException($"Line {LineNumber}: Invalid score format for '{Score}'. Must be an integer.");
+                    }
+
+                    // checks if the length of the fields are valid
+                    if (parts.Length < 3 || parts.Length > 3)
+                    {
+                        throw new MisiingFieldException($"Line {LineNumber}: Expected 3 fields but found {parts.Length}.");
+                    }
+
+
+                    // Try converting Score to an integer
+                    if (!int.TryParse(Score, out int score) || score < 0 || score > 100)
+                    {
+                        throw new InvalidScoreFormatException($"Line {LineNumber}: Invalid score format for '{Score}'. Must be an integer between 0 and 100.");
+                    }
+
+                    // Create a new Student object and add it to the list
+                    var student = new Student(id, FullName, score);
+                }
+            }
+            return students;
+        }
+    }
+}
